a583511a9a5592c6b1bba1bd418e8b0afe5ec5c1
mingri31164 <3116430062@qq.com>
2025-05-08 00:56:50 +0800

    📝chore：upload static files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["index.html", "./src/**/*.{html,js,ts,jsx,tsx,vue}"],
+    content: ["aiagent-index.html", "./src/**/*.{html,js,ts,jsx,tsx,vue}"],
     theme: {
         extend: {},
     },
