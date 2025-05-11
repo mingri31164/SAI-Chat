a43b9a8e32ab9cb554ad5bb33ee3191a9fea940b
mingri31164 <3116430062@qq.com>
2025-05-11 21:55:24 +0800

    📝file：optimize configuration file management

---
+++ chat-ai/src/main/java/com/mingri/ai/config/AIAutoConfig.java
@@ -0,0 +1,10 @@
+package com.mingri.ai.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@AutoConfiguration
+@ComponentScan(basePackages = {"com.mingri.ai"})
+public class AIAutoConfig {
+}
---
+++ chat-core/src/main/java/com/mingri/core/CoreAutoConfig.java
@@ -0,0 +1,12 @@
+package com.mingri.core;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@AutoConfiguration
+@ComponentScan(basePackages = "com.mingri.core")
+public class CoreAutoConfig {
+
+
+}
